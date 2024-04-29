e40bd8c9a6713fc5190fe98b268df7698a0d7847
Alister <Alister.moral@gmail.com>
2024-04-29 15:16:23 +0200

    detaille responsive

--- script.js
+++ script.js
@@ -11,21 +11,46 @@ function copyEmail() {
 
 document.addEventListener('DOMContentLoaded', function () {
     const menuIcon = document.getElementById('menu-icon');
+    const closeIcon = document.getElementById('close-icon');
     const navbar = document.getElementById('navbar');
 
+    closeIcon.style.display = 'none';
+
     menuIcon.addEventListener('click', function() {
+        menuIcon.classList.toggle('active');
         if (navbar.style.display === 'block') {
             navbar.style.display = 'none';
+            closeIcon.style.display = 'none'; 
+            menuIcon.style.display = 'block'; 
         } else {
             navbar.style.display = 'block';
+            closeIcon.style.display = 'block'; 
+            menuIcon.style.display = 'none'; 
         }
+    });
+
+    closeIcon.addEventListener('click', function() {
+        navbar.style.display = 'none';
+        closeIcon.style.display = 'none'; 
+        menuIcon.style.display = 'block'; 
     });
 
     window.addEventListener('resize', function() {
         if (window.innerWidth > 767.98) {
-            navbar.style.display = 'flex'; 
-        } else if (!menuIcon.contains(event.target)) {
+            navbar.style.display = 'flex';
+            if (menuIcon.classList.contains('active')) {
+                closeIcon.style.display = 'block'; 
+                menuIcon.style.display = 'none'; 
+            } else {
+                closeIcon.style.display = 'none'; 
+                menuIcon.style.display = 'block'; 
+            }
+        } else {
             navbar.style.display = 'none'; 
+            closeIcon.style.display = 'none'; 
+            menuIcon.style.display = 'block'; 
         }
     });
 });
+
+
